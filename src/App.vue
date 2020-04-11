a9d96eb4db83f3540015eed0bf97d77183790cb0
Jose Luis Assadi <joseluis@assadi.io>
2020-04-11 01:01:52 -0400

    configure application components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Calendar id="calendar" />
+    <Menu id="menu" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Calendar from "./components/Calendar.vue";
+import Menu from "./components/Menu.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Calendar,
+    Menu
   }
 })
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
+  width: 100%;
+  height: 95vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $font-color;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  column-gap: 1%;
+  grid-template-areas: "calendar menu";
+}
+#calendar {
+  grid-area: calendar;
+  width: 100%;
+  height: 100%;
+}
+#menu {
+  grid-area: menu;
+  width: 90%;
+  height: 100%;
 }
 </style>
