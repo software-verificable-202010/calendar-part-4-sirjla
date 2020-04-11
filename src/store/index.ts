8b4633d62b71ec294c77427504b34c6afd907563
Jose Luis Assadi <joseluis@assadi.io>
2020-04-11 01:00:11 -0400

    implement data store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,48 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex, { StoreOptions } from 'vuex';
+import { RootState } from '../types/store';
+import moment from "moment";
+import {
+  selectMonthAction,
+  selectYearAction
+} from "./ActionNames";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+const startingDayOfMonth = 1;
+
+const store: StoreOptions<RootState> = {
+  state: {
+    selectedYear: moment().year(),
+    selectedMonth: moment().month(),
+    currentDate: moment()
+  },
+  mutations: {
+    selectMonth(state, month: number) {
+      state.selectedMonth = month;
+    },
+    selectYear(state, year: number) {
+      state.selectedYear = year;
+    }
+  },
+  actions: {
+    selectMonth(context, month: number) {
+      context.commit(selectMonthAction, month);
+    },
+    selectYear(context, year: number) {
+      context.commit(selectYearAction, year);
+    }
+  },
+  modules: {},
+  getters: {
+    selecteddMonthStartingWeekNumber: (state): number => {
+      return moment().set({
+        year: state.selectedYear,
+        month: state.selectedMonth,
+        date: startingDayOfMonth
+      }).isoWeek();
+    }
+  }
+};
+
+export default new Vuex.Store<RootState>(store);
---
+++ src/types/store.d.ts
@@ -0,0 +1,11 @@
+import moment from "moment";
+
+interface RootState {
+  selectedYear: number;
+  selectedMonth: number;
+  currentDate: moment.Moment;
+}
+
+export {
+  RootState
+}
